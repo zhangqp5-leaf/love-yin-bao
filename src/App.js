87e5d75b9a2aec4343480e10bcfbb97807aa0e67
loquatlo <zhangqp5@mail3.sysu.edu.cn>
2021-03-08 14:47:30 +0800

    解决背景不充满&文字显示问题

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ var sectionStyle = {
     // width: '1920px',
     // height: '1080px',
     width: '100%',
-    height: '910px',
+    height: '940px',
     // backgroundSize: 'cover',
     backgroundImage: `url(${Background})`,
 };
@@ -53,21 +53,22 @@ export default class App extends Component {
         // console.log(day1)
         return (
             <div style={sectionStyle} className={styles.container}>
-                <div style={{boxShadow: '0 2px 16px 0 rgba(16, 54, 113, 16)'}}>
+                <div className={styles.appTotleStyle}>
                     <div className={styles.appTextStyle}>
-                        <p style={{display: 'inline-block', fontSize: '100%'}}>和尹宝在一起已经</p>
+                        <div style={{display: 'inline-block', fontSize: '100%'}}>和尹宝在一起</div>
+                        <div style={{display: 'inline-block', fontSize: '100%'}}>已经</div>
                     </div>
                     <div className={styles.appTimeStyle}>
-                        <p style={{display: 'inline-block', fontSize: '100%'}}>{day1}</p>
-                        <p style={{display: 'inline-block', fontSize: '35%'}}>天</p>
-                        <p style={{display: 'inline-block', fontSize: '100%'}}>{hour1}</p>
-                        <p style={{display: 'inline-block', fontSize: '35%'}}>小时</p>
-                        <p style={{display: 'inline-block', fontSize: '100%'}}>{minute1}</p>
-                        <p style={{display: 'inline-block', fontSize: '35%'}}>分钟</p>
+                        <div style={{display: 'inline-block', fontSize: '100%'}}>{day1}</div>
+                        <div style={{display: 'inline-block', fontSize: '35%'}}>天</div>
+                        <div style={{display: 'inline-block', fontSize: '100%'}}>{hour1}</div>
+                        <div style={{display: 'inline-block', fontSize: '35%'}}>小时</div>
+                        <div style={{display: 'inline-block', fontSize: '100%'}}>{minute1}</div>
+                        <div style={{display: 'inline-block', fontSize: '35%'}}>分钟</div>
                     </div>
                     <div className={styles.appSecondStyle}>
-                        <p style={{display: 'inline-block', fontSize: '100%'}}>{second1}</p>
-                        <p style={{display: 'inline-block', fontSize: '35%'}}>秒</p>
+                        <div style={{display: 'inline-block', fontSize: '100%'}}>{second1}</div>
+                        <div style={{display: 'inline-block', fontSize: '35%'}}>秒</div>
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import styles from './Index.module.less';
+import './Index.css';
 
 ReactDOM.render(
     <React.StrictMode>
