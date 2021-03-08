dee2a15ac705cc28991171aa5f94ba1881d9ea17
loquatlo <zhangqp5@mail3.sysu.edu.cn>
2021-03-08 20:50:22 +0800

    添加背景切换动画，优化button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import {Button} from 'antd';
+import {CSSTransition} from 'react-transition-group';
 
 import styles from './App.module.less';
+import './App.css';
 import Background1 from './img/1037400.png';
 import Background2 from './img/wallhaven-g85vp3.jpg';
 import Background3 from './img/1033113.png';
 import Background4 from './img/499590.jpg';
 import Background5 from './img/501188.jpg';
+import Background6 from './img/2032006.jpg';
+import Background7 from './img/微信图片_20200728103045.jpg';
+import Background8 from './img/微信图片_20200728103054.jpg';
 
-// const backgroundList = [Background1, Background2, Background3, Background4, Background5];
 
 export default class App extends Component {
 
@@ -16,8 +20,9 @@ export default class App extends Component {
         super(props);
         this.state = {
             date: new Date(),
+            isShowBackground: false,
             Background: Background2,
-            backgroundList: [Background1, Background2, Background3, Background4, Background5],
+            backgroundList: [Background1, Background2, Background3, Background4, Background5, Background6, Background7, Background8],
         };
         this.handleClickChangeBackground = this.handleClickChangeBackground.bind(this);
     }
@@ -50,7 +55,9 @@ export default class App extends Component {
         let BackgroundCopy = this.state.Background;
         this.setState({
             Background: this.state.backgroundList[Math.floor(Math.random() * this.state.backgroundList.length)],
+            // Background: this.state.Background,
             backgroundList: [...this.state.backgroundList, BackgroundCopy],
+            isShowBackground: !this.state.isShowBackground,
         });
     }
 
@@ -71,26 +78,39 @@ export default class App extends Component {
         // console.log(day1)
         // console.log(this.state.backgroundList);
         // console.log(this.state.Background);
+        // console.log(this.state.isShowBackground);
         return (
-            <div className={styles.container} style={sectionStyle}>
-                <div className={styles.appTotleStyle}>
-                    <div className={styles.appTextStyle}>
-                        <div style={{display: 'inline-block', fontSize: '100%'}}>和尹宝在一起</div>
-                        <div style={{display: 'inline-block', fontSize: '100%'}}>已经</div>
+            <div>
+                <CSSTransition
+                    in={this.state.isShowBackground}
+                    timeout={500}
+                    classNames='fade'
+                    onEntered={() => {this.setState({
+                        isShowBackground: !this.state.isShowBackground,
+                    });}}
+                >
+                    <div style={sectionStyle}></div>
+                </CSSTransition>
+                <div className={styles.container}>
+                    <div className={styles.appTotleStyle}>
+                        <div className={styles.appTextStyle}>
+                            <div style={{display: 'inline-block', fontSize: '100%'}}>和尹宝在一起</div>
+                            <div style={{display: 'inline-block', fontSize: '100%'}}>已经</div>
+                        </div>
+                        <div className={styles.appTimeStyle}>
+                            <div style={{display: 'inline-block', fontSize: '100%'}}>{day1}</div>
+                            <div style={{display: 'inline-block', fontSize: '35%'}}>天</div>
+                            <div style={{display: 'inline-block', fontSize: '100%'}}>{hour1}</div>
+                            <div style={{display: 'inline-block', fontSize: '35%'}}>小时</div>
+                            <div style={{display: 'inline-block', fontSize: '100%'}}>{minute1}</div>
+                            <div style={{display: 'inline-block', fontSize: '35%'}}>分钟</div>
+                        </div>
+                        <div className={styles.appSecondStyle}>
+                            <div style={{display: 'inline-block', fontSize: '100%'}}>{second1}</div>
+                            <div style={{display: 'inline-block', fontSize: '35%'}}>秒</div>
+                        </div>
+                        <Button onClick={this.handleClickChangeBackground}>爱我你就点点我</Button>
                     </div>
-                    <div className={styles.appTimeStyle}>
-                        <div style={{display: 'inline-block', fontSize: '100%'}}>{day1}</div>
-                        <div style={{display: 'inline-block', fontSize: '35%'}}>天</div>
-                        <div style={{display: 'inline-block', fontSize: '100%'}}>{hour1}</div>
-                        <div style={{display: 'inline-block', fontSize: '35%'}}>小时</div>
-                        <div style={{display: 'inline-block', fontSize: '100%'}}>{minute1}</div>
-                        <div style={{display: 'inline-block', fontSize: '35%'}}>分钟</div>
-                    </div>
-                    <div className={styles.appSecondStyle}>
-                        <div style={{display: 'inline-block', fontSize: '100%'}}>{second1}</div>
-                        <div style={{display: 'inline-block', fontSize: '35%'}}>秒</div>
-                    </div>
-                    <Button onClick={this.handleClickChangeBackground}>点我</Button>
                 </div>
             </div>
         );
