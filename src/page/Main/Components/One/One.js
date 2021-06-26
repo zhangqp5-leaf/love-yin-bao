f6eda30dec9de93a22d614589238fcd0c5b5f288
Leaf5Zhang <zhangqp5@mail3.sysu.edu.cn>
2021-06-26 22:28:33 +0800

    更改每日一句URI

--- src/page/Main/Components/One/One.js
+++ src/page/Main/Components/One/One.js
@@ -1,30 +1,83 @@
-import React, {useState, useEffect} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import axios from 'axios';
 
 import styles from './One.module.less';
 
-const oneURI = 'https://api.xygeng.cn/one';
+const oneURI = 'https://api.vvhan.com/api/love?type=json';
 
-const One = () => {
-    const [oneSentance, setOneSentance] = useState('');
+class One extends Component {
 
-    useEffect(() => {
+    state = {
+        oneSentance: '',
+    };
+
+    componentDidMount() {
+        this.getOneSentance();
+    }
+
+    getOneSentance = () => {
         var config = {
             method: 'get',
             url: oneURI,
         };
         axios(config)
             .then(response => {
-                setOneSentance(response.data.data.content);
+                console.log(response.data.ishan);
+                // setOneSentance(response.data.ishan);
+                this.setState({oneSentance: response.data.ishan});
             })
             .catch(error => {
                 console.log(error);
             });
-    });
+    }
 
-    return (
-        <div className={styles.onePanal}>{oneSentance}</div>
-    );
-};
+    render() {
+        const {oneSentance} = this.state;
+        return (
+            <div className={styles.onePanal}>{oneSentance}</div>
+        );
+    }
+}
+
+// const One = () => {
+//     const [oneSentance, setOneSentance] = useState('');
+
+//     // useEffect(() => {
+//     //     let timeID = setInterval(() => {
+//     //         var config = {
+//     //             method: 'get',
+//     //             url: oneURI,
+//     //         };
+//     //         axios(config)
+//     //             .then(response => {
+//     //                 console.log(response.data.ishan);
+//     //                 // setOneSentance(response.data.ishan);
+//     //             })
+//     //             .catch(error => {
+//     //                 console.log(error);
+//     //             });
+//     //     }, 1000);
+//     //     return () => clearInterval(timeID);
+//     // });
+
+//     useEffect(() => {
+//         var config = {
+//             method: 'get',
+//             url: oneURI,
+//         };
+//         axios(config)
+//             .then(response => {
+//                 console.log(response.data.ishan);
+//                 // setOneSentance(response.data.ishan);
+//             })
+//             .catch(error => {
+//                 console.log(error);
+//             });
+//     });
+
+//     return (
+//         <div className={styles.onePanal}>{oneSentance}</div>
+//     );
+// };
 
 export default One;
